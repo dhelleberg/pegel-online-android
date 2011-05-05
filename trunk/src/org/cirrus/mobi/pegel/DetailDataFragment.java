ebd62f96e743d663eb10821916ce933bc92d3836
dominik.helleberg@googlemail.com <dominik.helleberg@googlemail.com@c47dbf8a-084e-f88e-8da7-9a22b171f03c>
2011-05-05 19:00:24 +0000

    added license

--- trunk/src/org/cirrus/mobi/pegel/DetailDataFragment.java
+++ trunk/src/org/cirrus/mobi/pegel/DetailDataFragment.java
@@ -1,5 +1,24 @@
 package org.cirrus.mobi.pegel;
 
+/*	Copyright (C) 2011	Dominik Helleberg
+
+This file is part of pegel-online.
+
+pegel-online is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pegel-online is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pegel-online.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
