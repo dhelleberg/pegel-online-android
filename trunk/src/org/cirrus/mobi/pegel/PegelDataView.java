fff0aea3e380bb84fc261890828619d523a34b89
dominik.helleberg <dominik.helleberg@c47dbf8a-084e-f88e-8da7-9a22b171f03c>
2011-05-22 19:43:35 +0000

    refactored common functionality

--- trunk/src/org/cirrus/mobi/pegel/PegelDataView.java
+++ trunk/src/org/cirrus/mobi/pegel/PegelDataView.java
@@ -18,61 +18,27 @@ You should have received a copy of the GNU General Public License
 along with pegel-online.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import java.util.Set;
-
-import org.cirrus.mobi.pegel.widget.PegelWidgetProvider.UpdateService;
-
-import android.app.Activity;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class PegelDataView extends Activity {
+public class PegelDataView extends AbstractPegelDetailsActivity {
 
 	protected static final String TAG = "PegelDataView";
-
-	private static final int DIALOG_ABOUT = 1;
-
-	private PegelApplication pa;
-	private String app_ver;
-
-	private String pnr = null;
-	private boolean norefresh = false;
-
-	private PegelDataProvider pegelDataProvider;
-
-	private PegelDetailHelper pegelDetailHelper;
-
 	
-	
+
+		
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		getWindow().requestFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-
+		
 		setContentView(R.layout.pegel_data);	
 		
-		this.pa = (PegelApplication) getApplication();
-		
+		pa = (PegelApplication) getApplication();
 		pa.tracker.trackPageView("/PegelDataView");	
 		
 		this.pnr = getIntent().getStringExtra("pnr");
@@ -94,101 +60,10 @@ public class PegelDataView extends Activity {
 		this.pegelDetailHelper = new PegelDetailHelper(this);
 
 		setProgressBarIndeterminateVisibility(true);
-		this.pegelDataProvider.showData(pnr, pegelDetailHelper.pdrData, pegelDetailHelper.pdrImage, pegelDetailHelper.pdrDataDetails);
-				
-		try {
-			this.app_ver = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
-		} catch (NameNotFoundException e) {
-			this.app_ver = "unknown";
-		}
+		this.pegelDataProvider.showData(pnr, pegelDetailHelper.pdrData, pegelDetailHelper.pdrImage, pegelDetailHelper.pdrDataDetails, null, 0);
 
 	}
 
 
-	//Menu Inflater for fixture selection
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.layout.detailmenu, menu);
-		return true;
-	}
-
-	// This method is called once a menu item is selected
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.m_refresh:
-			this.pegelDataProvider.refresh(pnr, pegelDetailHelper.pdrData, pegelDetailHelper.pdrImage, pegelDetailHelper.pdrDataDetails);
-			this.pa.tracker.trackEvent("PegelDataView", "refresh", "refresh", 1);
-			return true;
-		case R.id.m_feedback:
-			final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-			emailIntent.setType("message/rfc822");
-			emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"dominik.helleberg@googlemail.com"});
-			emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Pegel-Online Feedback" );  
-			this.pa.tracker.trackEvent("PegelDataView", "feedback", "feedback", 1);
-			startActivity(Intent.createChooser(emailIntent, "Email senden..."));
-			return true;
-		case R.id.m_about:
-			showDialog(DIALOG_ABOUT);
-			this.pa.tracker.trackEvent("PegelDataView", "about", "about", 1);
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-
-		}
-	}
-
-	@Override
-	protected Dialog onCreateDialog(int id) {
-	    Dialog dialog;
-	    switch(id) {
-	    case DIALOG_ABOUT:
-	        dialog = createAboutDialog();
-	        break;
-	    default:
-	        dialog = null;
-	    }
-	    return dialog;
-	}
-	
-	private Dialog createAboutDialog() {
-		
-		Dialog dialog = new Dialog(this);
-
-		dialog.setContentView(R.layout.about_dialog);
-		dialog.setTitle("About Pegel-Online v."+app_ver);
-
-		TextView text = (TextView) dialog.findViewById(R.id.about_d_text);
-		text.setText(R.string.about);
-		ImageView image = (ImageView) dialog.findViewById(R.id.about_d_logo);
-		image.setImageResource(R.drawable.icon);
-		
-		return dialog;
-	}
-
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if ((keyCode == KeyEvent.KEYCODE_BACK)) {
-			//delete preferences
-			SharedPreferences settings = getSharedPreferences("prefs", Context.MODE_WORLD_WRITEABLE);
-			SharedPreferences.Editor edit = settings.edit();
-			edit.clear();
-			edit.commit();
-			this.norefresh = true; //do not refresh service
-			
-		}
-		return super.onKeyDown(keyCode, event);
-	}
-	
-	@Override
-	protected void onStop() {
-		super.onStop();
-		// update the widget if we exit
-		if(!norefresh) 
-			this.startService(new Intent(this, UpdateService.class));
-		norefresh = false;
-	}
-	
 
 }
