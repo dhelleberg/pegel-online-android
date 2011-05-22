2be92f2417f1ae3b95062dc166e30f8bc6142262
dominik.helleberg <dominik.helleberg@c47dbf8a-084e-f88e-8da7-9a22b171f03c>
2011-05-22 19:43:57 +0000

    implemented image cache

--- src/org/cirrus/mobi/pegel/PegelApplication.java
+++ src/org/cirrus/mobi/pegel/PegelApplication.java
@@ -18,6 +18,9 @@ package org.cirrus.mobi.pegel;
     along with pegel-online.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.cirrus.mobi.pegel.data.PointStore;
 
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
@@ -28,14 +31,14 @@ import android.graphics.drawable.Drawable;
 public class PegelApplication extends Application {
 	
 	//for local testing
-	//public static final String host = "http://10.0.2.2:8888";
+//	public static final String host = "http://10.0.2.2:8888";
 	//for production
 	public static final String host = "http://pegel-online.appspot.com";
 	
 	private PointStore pointStore;
 	public GoogleAnalyticsTracker tracker;
 
-	private Drawable d;
+	private Map<String, Drawable> imageCache = null;
 
 
 	@Override
@@ -45,7 +48,9 @@ public class PegelApplication extends Application {
 		this.pointStore = new PointStore();
 		
 		tracker = GoogleAnalyticsTracker.getInstance();
-		tracker.start("UA-1395561-4",30 ,this); //every 30 seconds the data is send to analytics 
+		tracker.start("UA-1395561-4",30 ,this); //every 30 seconds the data is send to analytics
+		
+		this.imageCache = new HashMap<String, Drawable>();
 	}
 	
 	public PointStore getPointStore()
@@ -53,12 +58,12 @@ public class PegelApplication extends Application {
 		return this.pointStore;
 	}
 
-	public void setCachedImage(Drawable d) {
-		this.d = d;		
+	public void setCachedImage(String key, Drawable d) {
+		this.imageCache.put(key, d);
 	}
 
-	public Drawable getCachedDrawable() {
-		return d;
+	public Drawable getCachedDrawable(String key) {
+		return this.imageCache.get(key);
 	}
 
 }
