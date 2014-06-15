51d04db41aef1b1a9e3ad259e4f10901b24a611b
dominik.helleberg@gmail.com <dominik.helleberg@gmail.com@c47dbf8a-084e-f88e-8da7-9a22b171f03c>
2014-06-15 15:24:59 +0000

    added connection test, adjusted layouts for tablets, tried to wiget min width / height

--- trunk/tests/src/org/cirrus/mobi/pegel/tests/UIBBTests.java
+++ trunk/tests/src/org/cirrus/mobi/pegel/tests/UIBBTests.java
@@ -1,9 +1,10 @@
 package org.cirrus.mobi.pegel.tests;
-
+import com.squareup.spoon.Spoon;
 import junit.framework.Assert;
 
 import org.cirrus.mobi.pegel.SelectRiver;
-import org.cirrus.mobi.tests.tools.Screenshot;
+//import org.cirrus.mobi.tests.tools.Screenshot;
+
 
 
 import android.content.Context;
@@ -14,85 +15,94 @@ import com.jayway.android.robotium.solo.Solo;
 
 public class UIBBTests extends ActivityInstrumentationTestCase2<SelectRiver>{
 
-	private Solo solo;
-	private SelectRiver mActivity;
+    private Solo solo;
+    private SelectRiver mActivity;
 
-	public UIBBTests() {
-		super("org.cirrus.mobi.pegel", SelectRiver.class);
-	}
+    public UIBBTests() {
+        super("org.cirrus.mobi.pegel", SelectRiver.class);
+    }
 
-	public void setUp() throws Exception {
-		
-		clearPrefs();
-		
-		mActivity = getActivity();
-		
-	    getInstrumentation().waitForIdleSync();
-		solo = new Solo(getInstrumentation(), mActivity);
+    public void setUp() throws Exception {
 
-	}
+        clearPrefs();
 
-	private void clearPrefs() {
-		SharedPreferences prefs = getInstrumentation().getTargetContext().getSharedPreferences("prefs", Context.MODE_WORLD_WRITEABLE);
-		SharedPreferences.Editor edit = prefs.edit();
-		edit.clear();
-		edit.commit();
-	}
+        mActivity = getActivity();
 
-	public void testSelection() throws Exception {
-		
-		try {
-			Assert.assertTrue(solo.waitForText("ALLER", 1, 8000));
-			solo.clickOnText("RHEIN");
-			Assert.assertTrue(solo.waitForText("BONN", 1, 8000));
-			solo.clickOnText("BONN");
-			Assert.assertTrue(solo.waitForText("Tendenz"));
-			solo.clickOnMenuItem("About");
-	
-			getInstrumentation().waitForIdleSync();
-			Screenshot.save_screenshot(solo.getCurrentActivity().getWindow(), "messung");
-	
-			Assert.assertTrue(solo.waitForText("Dominik"));		
-			solo.goBack();
-			
-			solo.clickOnMenuItem("Refresh");
-			
-			solo.clickOnMenuItem("Feedback");
-			assertTrue(solo.waitForText("Email senden..."));
-			
-			solo.goBack();
-			
-			solo.clickOnText("Details");
-			assertTrue(solo.waitForText("TUGLW"));
-			getInstrumentation().waitForIdleSync();
-			Screenshot.save_screenshot(solo.getCurrentActivity().getWindow(), "details");
-			
-			solo.clickOnText("Karte");
-			assertTrue(solo.waitForText("Karte"));
-			getInstrumentation().waitForIdleSync();
-			Screenshot.save_screenshot(solo.getCurrentActivity().getWindow(), "karte");
+        getInstrumentation().waitForIdleSync();
+        solo = new Solo(getInstrumentation(), mActivity);
 
-			
-		}
-		catch(Exception e)
-		{
-			Screenshot.save_screenshot(solo.getCurrentActivity().getWindow(), "ERROR"+e.getMessage().trim());
-			throw e;
-		}
-	}
+    }
 
-	@Override
-	public void tearDown() throws Exception {
+    private void clearPrefs() {
+        SharedPreferences prefs = getInstrumentation().getTargetContext().getSharedPreferences("prefs", Context.MODE_WORLD_WRITEABLE);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.clear();
+        edit.commit();
+    }
 
-		try {
-			solo.finalize();
-		} catch (Throwable e) {
+    public void testSelection() throws Exception {
 
-			e.printStackTrace();
-		}
-		getActivity().finish();
-		super.tearDown();
+        try {
+            Assert.assertTrue(solo.waitForText("ALLER", 1, 8000));
+            Spoon.screenshot(getActivity(), "river");
+            solo.clickOnText("RHEIN");
+            Assert.assertTrue(solo.waitForText("BONN", 1, 8000));
+            Spoon.screenshot(getActivity(), "station-select");
+            solo.clickOnText("BONN");
+            Assert.assertTrue(solo.waitForText("Ok"));
+            Spoon.screenshot(getActivity(), "popup1");
+            solo.clickOnButton("Ok");
+            Assert.assertTrue(solo.waitForText("Tendenz"));
+            Spoon.screenshot(getActivity(), "main-screen1");
+            solo.clickOnMenuItem("About");
 
-	}
+            getInstrumentation().waitForIdleSync();
+//			Screenshot.save_screenshot(solo.getCurrentActivity().getWindow(), "messung");
+            ;
+            Assert.assertTrue(solo.waitForText("Dominik"));
+
+            solo.goBack();
+
+            solo.clickOnMenuItem("Refresh");
+            Spoon.screenshot(getActivity(), "main-screen-refresh");
+            solo.clickOnMenuItem("Feedback");
+            assertTrue(solo.waitForText("Email senden..."));
+            Spoon.screenshot(getActivity(), "about");
+            solo.goBack();
+
+            solo.clickOnText("Details");
+            assertTrue(solo.waitForText("TuGLW"));
+            getInstrumentation().waitForIdleSync();
+            Spoon.screenshot(getActivity(), "details");
+//			Screenshot.save_screenshot(solo.getCurrentActivity().getWindow(), "details");
+
+            solo.clickOnText("Karte");
+            assertTrue(solo.waitForText("Karte"));
+            getInstrumentation().waitForIdleSync();
+//			Screenshot.save_screenshot(solo.getCurrentActivity().getWindow(), "karte");
+
+
+        }
+        catch(Exception e)
+        {
+//			Screenshot.save_screenshot(solo.getCurrentActivity().getWindow(), "ERROR"+e.getMessage().trim());
+            throw e;
+        }
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+
+        try {
+            solo.finalize();
+        } catch (Throwable e) {
+
+            e.printStackTrace();
+        }
+        getActivity().finish();
+        super.tearDown();
+
+    }
 
 }
+
